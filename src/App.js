be86b3d32ce7f0713749f0fc0a572e0b0c430e3d
dayvod <dyearwoodd@gmail.com>
2018-07-18 20:13:47 +0000

    Init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button innerText="Dog"/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// if we want button to be a link we must specifiy it is a button 
+const Button = props => {
+    return (
+        <button onClick={props.onClick} className={props.className}>{props.innerText}</button>    
+    )
+};
+
+Button.defaultProps = {
+    className: 'btn'
+};
+
+Button.propTypes = {
+    innerText: PropTypes.string.isRequired
+};
+
+export default Button;
